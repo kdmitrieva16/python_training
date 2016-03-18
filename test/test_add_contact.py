bcba8809f43c47c554d3f143b70dbfa0356ed7a5
kdmitrieva16 <dksu82@mail.ru>
2016-03-18 23:00:05 +0300

    Выделен класс для управления сессиями

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -12,7 +12,7 @@ def app(request):
 
 
 def test_add_new_contact(app):
-        app.login(username="admin", password="secret")
+        app.session.login(username="admin", password="secret")
         app.add_new_contact (Contact(firstname="Tony", middlename="nnnn", lastname="Wendice", nickname="mmm", title="mhfgj", company="gfhfghfgs", address="dfgdg dg dgd.d hdhfh",
                              home_phone="111111111", mobile_phone="22222222222", work_phone="44545456575", fax="hhfjjjfjfjfjfj", email2="fjfjghjhjjf@ddfdfdf.fg",
                              email3="gdfgfdfhfhfgh", homepage="ffsfsdgddfgdfgdfgdgghj", address2="wewwrweetehfghgf", phone2="hghfhfhfhhfjuiuidf",
@@ -20,7 +20,7 @@ def test_add_new_contact(app):
                              aday="//div[@id='content']/form/select[3]//option[11]",
                              amonth="//div[@id='content']/form/select[4]//option[3]", ayear="1999"))
         app.return_to_home_page()
-        app.logout()
+        app.session.logout()
 
 
 
