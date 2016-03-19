c948c39af462eadf0a9a00c0e0a2962d707d4f9b
kdmitrieva16 <dksu82@mail.ru>
2016-03-19 15:57:00 +0300

    Добавлен вспомогательный метод и тест для удаления контакта

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,7 +3,7 @@ from model.contact import Contact
 
 def test_add_new_contact(app):
         app.session.login(username="admin", password="secret")
-        app.contact.add_new (Contact(firstname="Tony", middlename="nnnn", lastname="Wendice", nickname="mmm", title="mhfgj", company="gfhfghfgs", address="dfgdg dg dgd.d hdhfh",
+        app.contact.add_new(Contact(firstname="Tony", middlename="nnnn", lastname="Wendice", nickname="mmm", title="mhfgj", company="gfhfghfgs", address="dfgdg dg dgd.d hdhfh",
                                      home_phone="111111111", mobile_phone="22222222222", work_phone="44545456575", fax="hhfjjjfjfjfjfj", email2="fjfjghjhjjf@ddfdfdf.fg",
                                      email3="gdfgfdfhfhfgh", homepage="ffsfsdgddfgdfgdfgdgghj", address2="wewwrweetehfghgf", phone2="hghfhfhfhhfjuiuidf",
                                      notes="sdfdgdfhdfhdfhfhjghjhgh", bday= "//div[@id='content']/form/select[1]//option[18]", bmonth="//div[@id='content']/form/select[2]//option[12]", byear= "1971",
