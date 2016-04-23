d2e47c236dc110bbb6b150b62ec95100ccbdaad8
kdmitrieva16 <dksu82@mail.ru>
2016-04-23 15:40:16 +0300

    Реализованы генераторы тестовых данных для групп и контактов

---
+++ data/contacts.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from model.contact import Contact
+
+testdata = [
+    Contact(firstname="Name 1", middlename="Name 2", lastname="Name 3", home_phone="11111111", work_phone="222222222222",
+            mobile_phone="3333333333")
+]
+
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,23 +1,8 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
-import pytest
-import random
-import string
 
-def random_string(prefix, maxlen):
-    symbols=string.ascii_letters + string.digits + string.punctuation + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-testdata = [Contact(firstname="", middlename="", lastname="")]+[
-    Contact(firstname=random_string("firstname",20), middlename=random_string("middlename", 20),
-            lastname=random_string("lastname", 20), home_phone=random_string("home",10), work_phone=random_string("work",10),
-            mobile_phone=random_string("mobile",10))
-    for i in range(5)
-]
-
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x)for x in testdata])
-
-def test_add_new_contact(app, contact):
+def test_add_new_contact(app, data_contacts):
+    contact=data_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.add_new(contact)
     assert len(old_contacts) + 1 == app.contact.count()
