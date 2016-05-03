0b12a4cb88d37da6cd44536239ba0538ac820b9a
kdmitrieva16 <dksu82@mail.ru>
2016-05-03 15:14:04 +0300

    Добавлена очистка firstname lastname  в тест test_contact_info

--- test/test_contact_info.py
+++ test/test_contact_info.py
@@ -1,5 +1,6 @@
 import re
 
+
 from random import  randrange
 
 
@@ -7,9 +8,9 @@ def test_contact_info_on_home_page(app):
     index=randrange(len(app.contact.get_contact_list()))
     contact_from_home_page=app.contact.get_contact_list()[index]
     contact_from_edit_page=app.contact.get_contact_info_from_edit_page(index)
-    assert contact_from_home_page.lastname==contact_from_edit_page.lastname
-    assert contact_from_home_page.firstname==contact_from_edit_page.firstname
-    assert contact_from_home_page.address==contact_from_edit_page.address
+    assert clear(contact_from_home_page.lastname)==clear(contact_from_edit_page.lastname)
+    assert clear(contact_from_home_page.firstname)== clear(contact_from_edit_page.firstname)
+    assert (contact_from_home_page.address)==contact_from_edit_page.address
     assert contact_from_home_page.all_phones_from_home_page==merge_phones_like_on_home_page(contact_from_edit_page)
     assert contact_from_home_page.all_emails_from_home_page==merge_emails_like_on_home_page(contact_from_edit_page)
 
